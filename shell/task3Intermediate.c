161edf27e52a0f77cd87b9479b71f564761b6b9f
Amina Merić <116023819+aminameric@users.noreply.github.com>
2023-04-07 16:25:01 +0200

    Update task3Intermediate.c

--- shell/task3Intermediate.c
+++ shell/task3Intermediate.c
@@ -43,3 +43,13 @@ int main(int argc, char *argv[]) {
     return 0;
 
 }
+/*
+-> kill() is a system call in Linux that sends a signal to a process or a group of processes. 
+The kill() system call takes two arguments: the process ID of the target process and the signal to be sent. kill() is often used in Unix-like systems to 
+communicate with running processes. For example, a process may use kill() to send a SIGTERM signal to a child process that is misbehaving or taking too long to 
+complete. The child process would then receive the signal and terminate.
+-> execv() is a system call used to execute a new program within the context of the current process. It is a variant of the exec(). 
+The execv() system call takes two arguments: the path of the new program to be executed and an array of pointers to null-terminated strings that 
+represent the program arguments. 
+
+*/
